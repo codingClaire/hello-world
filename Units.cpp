48700b836f09e3be56a44e97f0dfc5c201b5d6ec
codingClaire <suyueyinyin@126.com>
2018-06-18 16:00:40 +0800

    0.0.5
    长度转换基本实现

--- Units.cpp
+++ Units.cpp
@@ -12,25 +12,36 @@ class Unit {
 public:
 	Unit(double num, std::string unit) : data(num), unit(unit) {}
 	~Unit() {}
-	virtual double convert(Unit &obj);
-	virtual double _from(double num, std::string unit, std::map<std::string, int>& unitMap);
-	virtual double _to(double num, std::string unit, std::map<std::string, int>& unitMap);
+	virtual double convert(std::string unit) = 0;
+	virtual double _from()=0;
+	virtual double _to(double num, std::string unit)=0;
+	double getData(){
+		return data;
+	}
+	std::string getUnit(){
+		return unit;
+	}
 private:
 	double data;
 	std::string unit;
 };
 
 
-class Length : virtual public Unit {
-public:
-	Length(double num, std::string unit) : Unit(num, unit) {}
-    double _from(double num, std::string unit);
+//³¤¶È×ª»»
+class Length : virtual public Unit 
+{
+  public:
+	   Length(double num, std::string unit) : Unit(num, unit) {}
+	   ~Length() {}
+	   double convert(std::string toUnit);
+  private:
+    double _from();
 	double _to(double num, std::string unit);
-	~Length() {}
-private:
 	static std::map<std:: string, int> lenMap;
 
 };
+
+
 std::map<std::string, int>initial_map()
 {
 	std::map<std::string, int> tmp;
@@ -50,35 +61,46 @@ std::map<std::string, int>initial_map()
 std::map<std::string, int> Length::lenMap(initial_map());
 
 
-double Length::_from(double num, std::string unit) {
-	switch (lenMap.at(unit)) {
-	case 1:break;
-	case 2:return 1000 * num; break;
-	case 3:return 1609.3448*num; break;
-	case 4:return 0.3048*num; break;
-	case 5:return 0.2254*num; break;
-	case 6:return 0.001*num; break;
-	case 7:return 0.01*num; break;
+double Length::_from() 
+{
+	switch (lenMap.at(Unit::getUnit()))
+	{
+	case 1:return Unit::getData();
+	case 2:return 1000 * Unit::getData();
+	case 3:return 1609.3448*Unit::getData();
+	case 4:return 0.3048*Unit::getData(); 
+	case 5:return 0.0254*Unit::getData();
+	case 6:return 0.001*Unit::getData(); 
+	case 7:return 0.01*Unit::getData();
 	}
 }
-double Length::_to(double num, std::string unit) {
-	switch (lenMap.at(unit)) {
-	case 1:break;
-	case 2:return num/1000; break;
-	case 3:return num/1609.3448; break;
-	case 4:return num/0.3048; break;
-	case 5:return num/0.2254; break;
-	case 6:return num/0.001; break;
-	case 7:return num/0.01; break;
+double Length::_to(double num, std::string unit) 
+{
+	switch (lenMap.at(unit)) 
+	{
+	case 1: return num;
+	case 2:return num/1000; 
+	case 3:return num/1609.3448;
+	case 4:return num/0.3048;
+	case 5:return num/0.0254;
+	case 6:return num/0.001;
+	case 7:return num/0.01;
 	}
 }
+
+double Length::convert(std::string toUnit) 
+{
+	return _to(this->_from(), toUnit);
+}
+
+
 
 int main()
 {
 	std::string unit1, unit2;
-	double num;
+    double num;
 	std::cin >> num >> unit1 >> unit2;
-	std::cout <<Length::_from(num,unit1)<< std::endl;
+	Length a(num, unit1);
+	std::cout << a.convert(unit2) << std::endl;
 	return 0;
 } 
-
